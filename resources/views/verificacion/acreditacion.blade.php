8010c524bd719696e486d157f231f6053ef698b6
hxrxd <kevhar10@gmail.com>
2023-07-10 19:35:27 -0600

    change img

--- resources/views/verificacion/acreditacion.blade.php
+++ resources/views/verificacion/acreditacion.blade.php
@@ -11,9 +11,10 @@
 <body>
 
 
-    <br>
-    <br>
 <img src="assets/img/carta.jpg" alt="" width="750px" height="200px">
+
+    <br>
+    <br>
 
 <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:normal;font-size:15px;font-family:"Calibri",sans-serif;text-align:right;'><strong><span style='font-size:16px;font-family:"Corbel",sans-serif;'>Guatemala 30 de agosto del 2023</span></strong></p>
 <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:normal;font-size:15px;font-family:"Calibri",sans-serif;'><strong><span style='font-size:16px;font-family:"Corbel",sans-serif;'>Se&ntilde;or/a&nbsp;</span></strong></p>
@@ -50,5 +51,12 @@
 </table>
 <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:normal;font-size:15px;font-family:"Calibri",sans-serif;text-align:justify;'><span style='font-size:16px;font-family:"Corbel",sans-serif;'>&nbsp;</span></p>
 <p style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;line-height:normal;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;font-family:"Corbel",sans-serif;'>Agradeciendo su atenci&oacute;n y apoyo, me despido con muestras de mi alta estima.</span></p>
+
+    <br>
+    <br>
+
+<img src="assets/img/firma.jpg" alt="" width="750px" height="200px">
+
+
 </body>
 </html>
