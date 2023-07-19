71355172e454676b974a20976ef227b4c3c43adf
hxrxd <kevhar10@gmail.com>
2023-07-18 19:26:48 -0600

    add rout to redirect in local

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/seedbed/public/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
