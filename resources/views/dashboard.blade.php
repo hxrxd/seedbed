4aae8dfa074b4a84412eeebbea4808731ab9b9d1
hxrxd <kevhar10@gmail.com>
2023-07-30 19:24:11 -0600

    quitar editar fiscal del dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -122,11 +122,7 @@
                             </div>
                             <p class="text-white mb-4">Estado: <strong>Verificado</strong></p>
                             <div class="flex flex-row items-center justify-start">
-                                <a href="fiscal/{{ Auth::user()->email}}/edit" class="text-indigo-800 font-extrabold bg-[#e9f877] hover:bg-[#f7fdcf] rounded-lg text-sm px-2 py-1.5 text-center">
-                                    <svg class="w-5 h-5 text-indigo-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.109 17H1v-2a4 4 0 0 1 4-4h.87M10 4.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm7.95 2.55a2 2 0 0 1 0 2.829l-6.364 6.364-3.536.707.707-3.536 6.364-6.364a2 2 0 0 1 2.829 0Z"/>
-                                    </svg>
-                                </a>
+                            {{-- Editar --}}
                                 <a id="inscrib" href="verificacion/{{ Auth::user()->email}}" class="ml-2 text-indigo-800 font-extrabold bg-[#e9f877] hover:bg-[#f7fdcf] rounded-lg text-sm px-5 py-1.5 text-center">ID Virtual</a>
                             </div>
                         </div>
