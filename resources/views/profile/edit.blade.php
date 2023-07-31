ef7929a664f03f12858354a199cd7890ded8f265
hxrxd <kevhar10@gmail.com>
2023-07-31 17:00:31 -0600

    quitar modificar correo

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -13,11 +13,11 @@
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            {{--  <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
-            </div>
+            </div>--}}
 
             {{-- div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
