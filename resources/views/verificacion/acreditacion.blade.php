e5ee12be0ae22cd4236898c009243f3026a660f6
hxrxd <kevhar10@gmail.com>
2023-08-16 16:18:06 -0600

    add firma en blanco

--- resources/views/verificacion/acreditacion.blade.php
+++ resources/views/verificacion/acreditacion.blade.php
@@ -64,5 +64,7 @@
 
 
 <img src="data:image/png;base64, {!! $qrcode !!}">
+
+<p style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;line-height:normal;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:12px;font-family:"Corbel",sans-serif;'>Fundamento Legal Firma Faccimilar: Acuerdo Tribunal Supremo Electoral 977-2023</span></p>
 </body>
 </html>
